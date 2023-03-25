13b3e963a31757126d2656acb58f5ebbc5e9e0a6
Milind <120738859+TechWizardotexe@users.noreply.github.com>
2023-03-25 21:15:36 +0530

    Add files via upload

---
+++ lib/screen/pages/calls.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class calls extends StatefulWidget {
+  const calls({super.key});
+
+  @override
+  State<calls> createState() => _callsState();
+}
+
+class _callsState extends State<calls> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("jaini dalega"),
+      ),
+    );
+  }
+}
